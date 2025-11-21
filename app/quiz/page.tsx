65f59a838483774a47daf5458e0c1afa6c7278ef
Akshansh Patel <akshanshpatel003@gmail.com>
2025-11-21 14:35:00 +0530

    adding functionalities

--- app/quiz/page.tsx
+++ app/quiz/page.tsx
@@ -1,9 +1,7 @@
-import Header from "@/components/ui/Header"
-export default function Quiz() {
-    return (
-        <main className="px-5 flex flex-col item-center justify-center">
-            
-            
-        </main>
+export default function page(){
+    return(
+        <>
+        <h1>Hello quiz page</h1>
+        </>
     )
 }

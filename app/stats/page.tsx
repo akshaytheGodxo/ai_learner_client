aa53df4bee14108bbb4825138c7243e0f1e3439c
akshaytheGodxo <akshaysinghc230@gmail.com>
2025-11-21 13:12:19 +0530

    Akshansh Tasks

---
+++ app/stats/page.tsx
@@ -0,0 +1,21 @@
+const fakeUserData = {
+    name : "Akshay",
+    correct : 40,
+    incorrect: 60,
+    attempted: 100,
+    github_url: "",
+}
+
+/* 
+    @Akshansh tasks: 
+        1. mape the fakeuserdata to display a bargraph, u can generate even more faker data for now
+        2. Make the page look good
+*/
+
+export default function UserStats() {
+    return (
+        <main>
+            
+        </main>
+    )
+}
